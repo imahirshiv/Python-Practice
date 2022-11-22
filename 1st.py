9a1f5653b050a9674cefa381a2e6524118efcfd0
Ahir Shiv <imahirshiv@gmail.com>
2022-11-22 08:37:49 +0530

    Some python programs

---
+++ 1st.py
@@ -0,0 +1,13 @@
+import random
+print(random.randrange(1,100))
+x=5
+y={"apple","mango","banana"}
+z={"name":"Ahir","age":21}
+a=("ram","name","ageit")
+b=["apple","banana","mango","cherry"]
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
