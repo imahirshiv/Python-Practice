8e161a838858ba9654718af51afc8c6767383a22
Ahir Shiv <imahirshiv@gmail.com>
2022-11-22 13:19:32 +0530

    intial commit

---
+++ Operators/arithmetic.py
@@ -0,0 +1,9 @@
+a=15
+b=10
+print(a+b) #25
+print(a-b)  #5
+print(a*b) #150
+print(a/b)  #1.5    
+print(a%b)  #5
+print(a**b) 
+print(a//b) #1
---
+++ string/boolean.py
@@ -0,0 +1,8 @@
+print(10>9)
+
+a=50
+b=30
+if a>b:
+    print("Yes you are right ")
+else:
+    print("No b is grater from a")
