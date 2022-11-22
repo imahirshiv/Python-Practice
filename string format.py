b5a4a280ae4974c5a97c743983a5e4ad90ed5825
Ahir Shiv <imahirshiv@gmail.com>
2022-11-22 10:34:38 +0530

    intial commit

---
+++ string format.py
@@ -0,0 +1,6 @@
+x="I want a {0} ball and {1} bat and {2} mobiles ! Thank you for this {3} gifts for me!"
+ball=1
+bat=2
+mobile=3
+total=6
+print(x.format(ball,bat,mobile,total))
---
+++ string method.py
@@ -0,0 +1,9 @@
+a="This is new life and we have to follow the sprituals in life of anyhow!!"
+print(a.upper())
+print(a.lower())
+print(a.split(","))
+print(a.strip())
+print(a.replace("l", "d"))
+x="this is Ahir"
+y="Shivkumar"
+print( x + " " +y )
